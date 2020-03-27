59e0d210c8b178cc3ddbbf68138276e66b7ad2e5
junichi.enomoto <junichi.enomoto@tokyo.supersoftware.co.jp>
2020-03-27 19:13:21 +0900

    update_test

--- src/test/java/com/example/test/app/inquiry/InquiryControllerTest.java
+++ src/test/java/com/example/test/app/inquiry/InquiryControllerTest.java
@@ -1,23 +1,22 @@
-
 package com.example.test.app.inquiry;
 
-import com.example.test.DemoApplication;
-import com.example.test.app.inquiry.InquiryController;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest(classes = DemoApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+// @WebMvcTest
+@SpringBootTest
 public class InquiryControllerTest{
     
-    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
@@ -30,11 +29,27 @@ public class InquiryControllerTest{
 
     @Test
     public void getIndexTest() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/inquiry"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("index"));
+        // performでGETリクエストを送信している
+        mockMvc.perform(get("/inquiry"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index"));
     }
+
+    @Test
+    public void postIndexTest() throws Exception{
+        mockMvc.perform(post("/inquiry"))
+            .andExpect(status().isMethodNotAllowed());
+    }
+
+    @Test
+    public void postConfirmTest() throws Exception{
+        mockMvc.perform(post("/inquiry/confirm"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("confirm"));
+    }
+
+    
+
+
 }
-
-
 
